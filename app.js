8dd52cb8a22547ac2216079a24a20d9a01fda8f5
kiqa <kellyalanya@hotmail.com>
2015-07-03 23:53:14 -0400

    it is working.

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-j
+function animals(ward){
+    var animals= ["images/kelly.JPG", "images/ian3.JPG", "images/aleya44.jpg", "images/Qantu2.JPG"];
+    var i = Math.floor(Math.random()*animals.length);
+    var placeholder = document.getElementById("placeholder");
+    placeholder.setAttribute("src",animals[i]);
+}

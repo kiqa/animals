751461b8b674160052fae882bbcb0aa005e585e7
kiqa <kellyalanya@hotmail.com>
2015-07-04 00:21:21 -0400

    new add

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-function animals(ward){
+function animals(){
     var animals= ["images/kelly.JPG", "images/ian3.JPG", "images/aleya44.jpg", "images/Qantu2.JPG"];
     var i = Math.floor(Math.random()*animals.length);
-    var placeholder = document.getElementById("placeholder");
-    placeholder.setAttribute("src",animals[i]);
+    var payaso = document.getElementById("nariz");
+    payaso.setAttribute("src",animals[i]);
+}
+function fruit(){
+	alert("hi");
 }
